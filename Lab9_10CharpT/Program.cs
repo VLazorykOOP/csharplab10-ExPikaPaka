056714f3138d64e2114966d2d2153b397cf36d48
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-02-24 23:03:10 +0200

    Lab_10 Done

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,200 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿using Lab9_10CharpT;
+using System;
+using System.Collections;
+using System.Drawing;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Lab9_10CSharpT {
+    internal class Program {
+        static void Main(string[] args) {
+            int number = 1;
+
+            while (number != 0) {
+                Console.Write("Input task number [1-2], [0] to exit: ");
+
+                try {
+                    string? input = Console.ReadLine();
+
+                    if (input != null) {
+                        number = int.Parse(input);
+
+                        switch (number) {
+                            case 0:
+                                return;
+
+                            case 1:
+                                task1(); // Testing task 1
+                                break;
+
+                            case 2:
+                                task2(); // Testing task
+                                break;
+
+                            default:
+                                break;
+                        }
+                    } else {
+                        Console.WriteLine("Nothing provided. Exiting...");
+                    }
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+
+        static void task1() {
+            Console.WriteLine("|===~        Testing task 1.1        ~===|");
+
+            // Creating wrong shape rectangle
+            try { 
+                Console.WriteLine("Creating wrong shape rectangle:");
+                Lab9_10CharpT.Rectangle invalidRectangle = new Lab9_10CharpT.Rectangle(-5, 10);
+
+            } catch (InvalidRectangleException ex) {
+                Console.WriteLine($" Invalid rectangle: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+
+            // Creating DivideByZeroException
+            try { 
+                Console.WriteLine("Creating DivideByZero exception:");
+                int a = 0;
+                int b = 4;
+                int c = b / a;
+
+            } catch (DivideByZeroException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+
+            // Creating IndexOutOfRangeException
+            try {
+                Console.WriteLine("Creating IndexOutOfRange exception:");
+                int[] arr = new int[2];
+                int c = arr[3];
+
+            } catch (IndexOutOfRangeException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+
+            // Creating OutOfMemoryException
+            try {
+                Console.WriteLine("Creating OutOfMemory exception:");
+                int hugeAmount = int.MaxValue - 1;
+                int[] arr = new int[hugeAmount];
+
+            } catch (OutOfMemoryException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+
+            // Creating ArrayTypeMismatchException
+            try {
+                Console.WriteLine("Creating ArrayTypeMismatch exception:");
+                string[] names = { "Dog", "Cat", "Fish" };
+                Object[] objs = (Object[])names;
+
+                Object obj = (Object)13;
+                objs[2] = obj;
+
+            } catch (ArrayTypeMismatchException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+            // Creating InvalidCastException
+            try {
+                Console.WriteLine("Creating InvalidCast exception:");
+                IConvertible conv = true;
+                Char ch = conv.ToChar(null);
+
+            } catch (InvalidCastException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+            // Creating OverflowException
+            try {
+                Console.WriteLine("Creating Overflow exception:");
+                int a = int.MaxValue;
+                int b = 1;
+                int result = checked(a + b);
+
+            } catch (OverflowException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+
+            // Creating StackOverflowException
+            try {
+                Console.WriteLine("Creating StackOverflow exception:");
+                
+                void func() {
+                   func();
+                }
+
+                //func();
+
+                throw new StackOverflowException();
+
+            } catch (StackOverflowException ex) {
+                Console.WriteLine($" Exception: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($" An error occurred: {ex.Message}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+        }
+
+        static void task2() {
+            Console.WriteLine("|===~        Testing task 2.1        ~===|");
+
+            void f() {
+                City city = new City();
+                city.CrimeReported += (sender, e) =>
+                {
+                    Console.WriteLine($"Crime reported at {e.Location}: {e.Description}");
+                };
+
+                city.ReportCrime("Main Street", "Theft");
+            }
+            Task t = new Task(f);
+            t.Start();
+            Console.WriteLine("Text for paralel computation check");
+
+            t.Wait();
+            Console.WriteLine();
+        }
+
+    }
+}
+
+
+
 
---
+++ Lab9_10CharpT/Task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_10CharpT {
+    public class City {
+        public event EventHandler<CrimeEventArgs> CrimeReported;
+
+        public void ReportCrime(string location, string description) {
+            OnCrimeReported(new CrimeEventArgs(location, description));
+        }
+
+        protected virtual void OnCrimeReported(CrimeEventArgs e) {
+            CrimeReported?.Invoke(this, e);
+        }
+    }
+
+    public class CrimeEventArgs : EventArgs {
+        public string Location { get; }
+        public string Description { get; }
+
+        public CrimeEventArgs(string location, string description) {
+            Location = location;
+            Description = description;
+        }
+    }
+}
